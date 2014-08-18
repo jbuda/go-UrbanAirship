40113da804335b21e4b3d3fa14309bf1d346ae21
janusz <janusz@gfm.co.uk>
2014-08-18 11:51:15 +0100

    Working device lookup

--- device_token_lookup.go
+++ device_token_lookup.go
@@ -5,21 +5,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const APP_KEY = "xx"
 const MASTER_SECRET = "xx"
-const CPN = "AAAA000147028"
+const CPN = "xx"
 const API_URL = "go.urbanairship.com/api/device_tokens"
+const LIMIT = "10000"
+
+var devices []Device_info
 
 type Feed struct {
-	Device_tokens []Device_info
+	Next_page                  string
+	Device_tokens_count        int
+	Active_device_tokens_count int
+	Device_tokens              []Device_info
 }
 
 type Device_info struct {
 	Device_token string
 	Active       string
 	Alias        string
+	Tags         []string
 }
 
 func main() {
@@ -28,14 +36,15 @@ func main() {
 	fmt.Printf("CPN : " + CPN + "\n")
 	fmt.Printf("----------------\n")
 
-	load_json()
+	urlStr := fmt.Sprintf("https://%v:%v@%v?limit=%v", APP_KEY, MASTER_SECRET, API_URL, LIMIT)
+
+	load_json(urlStr)
 }
 
-func load_json() {
+func load_json(urlstring string) {
 
-	urlStr := fmt.Sprintf("https://%v:%v@%v", APP_KEY, MASTER_SECRET, API_URL)
 	client := &http.Client{}
-	r, _ := http.NewRequest("GET", urlStr, nil)
+	r, _ := http.NewRequest("GET", urlstring, nil)
 
 	resp, _ := client.Do(r)
 	body, _ := ioutil.ReadAll(resp.Body)
@@ -43,28 +52,30 @@ func load_json() {
 	var data Feed
 	json.Unmarshal(body, &data)
 
-	for i, device := range data.Device_tokens {
-		fmt.Printf("%d: token ~ %s alias ~ %s\n", i, device.Device_token, device.Alias)
-	}
-}
+	fmt.Printf("\nRunning %v", urlstring)
+	//fmt.Printf("\nNumber of device tokens : %d\nNumber of active device tokens : %d\nNext page : %s\n\n====\n\n", data.Device_tokens_count, data.Active_device_tokens_count, data.Next_page)
 
-/*
-{
-   "next_page": "https://go.urbanairship.com/api/device_tokens/?start=07AAFE44CD82C2F4E3FBAB8962A95B95F90A54857FB8532A155DE3510B481C13&limit=2",
-   "device_tokens_count": 87,
-   "device_tokens": [
-      {
-         "device_token": "0101F9929660BAD9FFF31A0B5FA32620FA988507DFFA52BD6C1C1F4783EDA2DB",
-         "active": false,
-         "alias": null,
-         "tags": []
-      },
-      {
-         "device_token": "07AAFE44CD82C2F4E3FBAB8962A95B95F90A54857FB8532A155DE3510B481C13",
-         "active": true,
-         "alias": null,
-         "tags": ["tag1", "tag2"]
-      }
-   ],
-   "active_device_tokens_count": 37
-}*/
+	for i, device := range data.Device_tokens {
+		_ = i
+		if device.Alias == CPN {
+			devices = append(devices, device)
+			fmt.Printf("%d\nDevice Token : %s\nAlias : %s\nTags : %s\n\n====\n\n", i, device.Device_token, device.Alias, strings.Join(device.Tags, ","))
+		}
+	}
+
+	if data.Next_page != "" {
+		urlStr := fmt.Sprintf("https://%v:%v@%v?%v", APP_KEY, MASTER_SECRET, API_URL, strings.Split(data.Next_page, "?")[1])
+		load_json(urlStr)
+	} else {
+
+		fmt.Printf("\nDevice tokens\n")
+
+		for _, el := range devices {
+			fmt.Printf("\n%v ~ %v", el.Device_token, el.Alias)
+		}
+
+		fmt.Printf("\n\n=========\nCompleted\n=========\n\n")
+
+	}
+
+}
